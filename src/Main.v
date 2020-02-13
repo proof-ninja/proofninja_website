ded1a854a88e238737b390af08acf2ac81e96441
Yoshihiro Imai <y.imai@proof-ninja.co.jp>
2020-02-13 14:08:46 +0900

    Add the clients

--- src/Main.v
+++ src/Main.v
@@ -33,6 +33,8 @@
 
  **** 主要取引先
  - #<a href="https://www.dailambda.jp/">ダイラムダ株式会社</a>#
+ - #<a href="http://www.nagoya-u.ac.jp/">国立大学法人 名古屋大学</a>#
+ - #<a href="https://www.nii.ac.jp/">国立情報学研究所</a>#
 
  **** 取引金融機関名
  - #<a href="https://gmo-aozora.com/">GMOあおぞらネット銀行</a>#
