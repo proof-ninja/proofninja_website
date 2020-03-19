97b8e856c81324b49635ab835527e602d8d14e68
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2020-03-19 12:40:59 +0900

    introduce english main page

---
+++ src/Main.v
@@ -0,0 +1,49 @@
+(**
+   #<a href="Main.html">English</a>#
+   #<a href="MainJa.html">日本語</a>#
+ *)
+(**
+ * proof ninja, Inc.
+
+  We develop proven software using formal verification tools and functional programming languages.
+
+ ** Business content
+ **** Software verification / development
+ Achieve highly reliable software development with functional programming and proof assistants.
+
+ **** Consulting
+ Consult us about the development of highly reliable software using formal verifications.
+
+ **** Seminar / Employee training
+ Technical seminars to learn proof assistants and functional programming.
+
+ ** Company Profile
+
+ **** Company Name
+ proof ninja, Inc.
+
+ **** CEO
+ Yoshihiro Imai
+ #<a href="https://yoshihiro503.github.io/home_en.html">profile</a>#
+
+ **** Location
+ 1-11-6 Minami Tokiwadai, Itabashi-ku, Tokyo, Japan
+
+ **** Establishment
+ September 02, 2019
+
+ **** Capital Stock
+ 11 million yen (including capital surplus)
+
+ **** Customers
+ - #<a href="https://www.dailambda.jp/">DaiLambda, Inc.</a>#
+ - #<a href="http://www.nagoya-u.ac.jp/">Nagoya University</a>#
+ - #<a href="https://www.nii.ac.jp/">National Institute of Infomatics</a>#
+
+ **** Financial Institution
+ - #<a href="https://gmo-aozora.com/">GMO Aozora Net Bank</a>#
+ - #<a href="https://www.bk.mufg.jp/">MUFG Bank</a>#
+
+ **** Contact
+ contact@proof-ninja.co.jp
+ *)
